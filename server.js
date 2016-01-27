b9854fd2962877dae966c830f30e99667442ac32
Tridex_Claude <claude.compere@tridexcreative.com>
2016-01-27 21:13:42 +0000

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PORT = 3001;
+var middleware = {
+  requireAuthentication: function(req, res, next){
+    console.log('Private route has been hit.');
+    next();
+  },
+  logger: function(req, res, next){
+    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+  res.send('This is the about page.');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+  console.log('Express Server has now started on port ' + PORT);
+});
