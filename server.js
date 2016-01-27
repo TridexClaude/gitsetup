723e667a4c68b2d7508ac5ac1a2c069917b1af50
Tridex_Claude <claude.compere@tridexcreative.com>
2016-01-27 21:18:03 +0000

    create middleware file

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = 3001;
-var middleware = {
-  requireAuthentication: function(req, res, next){
-    console.log('Private route has been hit.');
-    next();
-  },
-  logger: function(req, res, next){
-    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-    next();
-  }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
