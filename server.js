e0f5f6f57e1c7369b58b3804ffb7bd4f115a8325
Tridex_Claude <claude.compere@tridexcreative.com>
2016-01-27 22:06:26 +0000

    removed linked to middleware file

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3001;
-var middleware = require('./middleware.js');
+//var middleware = require('./middleware');
+var middleware = {
+  requireAuthentication: function(req, res, next){
+    console.log('Private route has been hit.');
+    next();
+  },
+  logger: function(req, res, next){
+    console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
 
 app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
